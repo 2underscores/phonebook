08b554712043bde2265f4004cf626c1685414be9
Crab <jeremys@cxnpl.com>
2024-10-11 13:00:39 +1100

    logging middleware - morgan

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const crypto = require('crypto')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -40,18 +42,19 @@ let personsData = [
   }
 ]
 
+app.use(express.json())
+morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 app.get('/info', (req, res) => {
-  console.log(`${req.method} ${req.path}`)
   res.send(`<p>Phonebook has info for ${personsData.length} people</p><p>${new Date()}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
-  console.log(`${req.method} ${req.path}`)
   res.json(personsData)
 })
 
 app.get('/api/persons/:id', (req, res)=>{
-  console.log(`${req.method} ${req.path}`)
   const id = req.params.id;
   const person = personsData.find(p=>p.id===id)
   if (person) {
@@ -61,12 +64,40 @@ app.get('/api/persons/:id', (req, res)=>{
   }
 })
 
+const generateId = () => {
+  return crypto.randomBytes(2).toString('hex');
+}
+
+const parsePerson = (p) => {
+  if (!p.name) {return {status: 'bad', msg: 'Must contain name'}}
+  if (!p.number) {return {status: 'bad', msg: 'Must contain number'}}
+  if (personsData.find(p2=>p2.name===p.name)) {return {status: 'bad', msg: 'Name already exists'}}
+  return resp = {status: 'ok', person: p}
+}
+
+app.post('/api/persons', (req, res)=>{
+  console.log(req.body)
+  const id = generateId();
+  const parse = parsePerson(req.body)
+  if (parse.status === 'bad') {
+    console.log(parse)
+    res.status(400).json(parse)
+    return
+  }
+  const person = {...parse.person, id:id}
+  personsData = personsData.concat(person) // TODO: Save to DB
+  res.json(person)
+})
+
 app.delete('/api/persons/:id', (req, res)=>{
-  console.log(`${req.method} ${req.path}`)
   const id = req.params.id;
   const person = personsData.find(p=>p.id===id)
-  // TODO: Delete
+  personsData = personsData.filter(p=>p.id!==id) // TODO: Delete DB
   res.status(person ? 204 : 404).end()
+})
+
+app.use((req, res, next)=> {
+  res.status(404).json({error: 'Does not exist'})
 })
 
 const PORT = 3001
