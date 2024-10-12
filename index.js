7504eb97785c11ef90d2edcdf208d95ec70d0519
Crab <jeremys@cxnpl.com>
2024-10-12 12:32:54 +1100

    Fly deployment of BE

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.use((req, res, next)=> {
   res.status(404).json({error: 'Does not exist'})
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
