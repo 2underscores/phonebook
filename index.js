824f6c2c7e5ce0cb909380ad2075b97cb5eaf218
Crab <jeremys@cxnpl.com>
2024-10-15 09:58:38 +1100

    Error handling middleware

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const app = express()
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
-morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+morgan.token('body', (req, res, next) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // Endpoints & Methods
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
   res.send(`<p>Phonebook has info for ${personsData.length} people</p><p>${new Date()}</p>`)
 })
 
@@ -26,38 +26,39 @@ const generateId = () => {
 }
 
 const parsePerson = (p) => {
-  if (!p.name) { return { status: 'bad', msg: 'Must contain name' } }
-  if (!p.number) { return { status: 'bad', msg: 'Must contain number' } }
+  if (!p.name) { return { error: 'ValidationError', status: 'bad', msg: 'Must contain name' } }
+  if (!p.number) { return { error: 'ValidationError', status: 'bad', msg: 'Must contain number' } }
   return resp = { status: 'ok', person: p }
 }
 
 // Person CRUD endpoints
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(result => {
-    res.json(result)
-  })
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(result => {res.json(result)})
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   // Parse request with standard body
   const parsed = parsePerson(req.body)
-  console.log('Attempting creation:', parsed.person);
   if (parsed.status === 'bad') {
-    console.log(parsed)
-    res.status(400).json(parsed)
+    next(parsed)
     return
   }
+  console.log('Attempting creation:', parsed.person);
   // If duplicate, error
-  const matchingPeople = Person.find({ // Should use countDocuments()
-    $or: [
-      { name: parsed.person.name },
-      { number: parsed.person.number }
-    ]})
+  const matchingPeople = Person.find( // Should use countDocuments()
+    {
+      $or: [
+        { name: parsed.person.name },
+        { number: parsed.person.number }
+      ]
+    })
     .then(people => {
       if (people.length > 0) {
-        console.log(`${people.length} Conflicts in DB:`, people)
-        res.status(400).json({ status: 'bad', msg: 'Name or number already exists' })
-        return Promise.reject('Duplicates')
+        console.log(`${people.length} Conflict(s) in DB:`, people)
+        next({ error: 'ValidationError', status: 'bad', msg: 'Name or number already exists' })
+        return
       } else {
         const person = new Person(parsed.person)
         return person.save()
@@ -67,32 +68,26 @@ app.post('/api/persons', (req, res) => {
       console.log('Saved:', savedPerson)
       res.json(savedPerson)
     })
-    .catch(err => {
-      console.log('Error:', err)
-    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
         console.log('Found:', person);
         res.json(person)
       } else {
-        res.status(404).end()
+        next({ error: 'NotFound', msg: 'Person ID not found' })
       }
     })
-    .catch(error => {
-      console.log('Error:', error);
-      res.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const parsed = parsePerson(req.body)
   if (parsed.status === 'bad') {
-    console.log(parsed)
-    res.status(400).json(parsed)
+    next(parsed)
     return
   }
   Person.findByIdAndUpdate(req.params.id, parsed.person, { new: true })
@@ -101,34 +96,42 @@ app.put('/api/persons/:id', (req, res) => {
         console.log('Updated:', updatedPerson)
         res.json(updatedPerson)
       } else {
-        res.status(404).end()
+        next({ error: 'NotFound', msg: 'Person ID not found' })
       }
     })
-    .catch(error => {
-      console.log('Error:', error)
-      res.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(result => {
       if (result) {
         console.log('Deleted:', result)
         res.status(204).end()
       } else {
-        res.status(404).end()
+        next({ error: 'NotFound', msg: 'Person ID not found' })
       }
     })
-    .catch(error => {
-      console.log('Error:', error)
-      res.status(500).end()
-    })
+    .catch(error => next(error))
 })
 
 app.use((req, res, next) => {
-  res.status(404).json({ error: 'Does not exist' })
+  res.status(404).json({ error: 'Path does not exist' })
 })
+
+errorHandler = (error, req, res, next) => {
+  console.error('Error: ', error)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.error === 'ValidationError') {
+    return res.status(400).json(error)
+  } else if (error.error === 'NotFound') {
+    return res.status(404).json(error)
+  }
+  console.log('Error unhandled. Fallback to default error handler')
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
